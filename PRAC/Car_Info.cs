d4ea457bddbfab85050f82e2835f41b407127469
Pratik <Pratik@DESKTOP-9189TTI>
2023-06-20 10:48:13 +0530

    Add project files.

---
+++ PRAC/Car_Info.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRAC
+{
+    public class Car_Info
+    {
+        private string name, brand;
+        private int mileage, price;
+
+        public Car_Info(string name, string brand , int mileage, int price=45000 )
+        {
+                this.name = name;
+            this.brand = brand;
+            this.mileage = mileage;
+            this.price = price;
+        }
+        public string PrintCar_Info() 
+        {
+            return($"Car name is {name} /  brand is {brand} / mileage is {mileage} / price is {price}");
+        }
+    }
+     
+	}
+	
+	
+
---
+++ PRAC/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRAC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // using optional parameter
+
+            Car_Info car = new Car_Info("Q7","audi",25);
+            Console.WriteLine(car.PrintCar_Info());
+
+            // using explicit value - override the value (i.e default price will change )
+
+            Car_Info car1 = new Car_Info("passat","volksvagon",60,100000);
+            Console.WriteLine(car1.PrintCar_Info());
+
+            // Using NAMED ARGUEMENTS 
+
+            Car_Info car2 = new Car_Info(name: "fortuner", brand: "toyota", mileage: 20, price: 3200000);
+            Console.WriteLine(car2.PrintCar_Info());
+        }
+
+    }
+}
